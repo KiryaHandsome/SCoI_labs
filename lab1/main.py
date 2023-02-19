2c36568cfc82c98b4a1400ecc6a46bbd97f7367f
kirya handsome <kirill19962003@gmail.com>
2023-02-20 00:06:47 +0300

    feat: add main.py

---
+++ lab1/main.py
@@ -0,0 +1,11 @@
+from even_numbers import generate_numbers
+from even_numbers import evens
+from hello_world import say_hello
+from caclulator import calculate
+
+
+if __name__ == '__main__':
+    say_hello()
+    print(calculate(12, 14, 'div'))
+    print(generate_numbers())
+    print(evens(generate_numbers()))
