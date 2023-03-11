c2279aa2ad8f3411aae07f41379133b0981b3970
kirya handsome <kirill19962003@gmail.com>
2023-03-11 23:12:30 +0300

    feat: implement count_non_decl_sentences() and test it

--- lab2/task1/statistic_util.py
+++ lab2/task1/statistic_util.py
@@ -3,7 +3,7 @@ from constants import SENTENCE_PATTERN, NON_DECLARATIVE_SENTENCE_PATTERN, WORD_P
     ONE_WORD_ABBREVIATIONS, TWO_WORDS_ABBREVIATIONS
 
 
-def count_sentences(text: str):
+def count_sentences(text: str) -> int:
     lower_text = text.lower()
     amount = len(re.findall(SENTENCE_PATTERN, lower_text))
 
@@ -16,4 +16,6 @@ def count_sentences(text: str):
     return amount
 
 
+def count_non_declarative_sentences(text: str) -> int:
+    return len(re.findall(NON_DECLARATIVE_SENTENCE_PATTERN, text.lower()))
 
--- lab2/task1/test.py
+++ lab2/task1/test.py
@@ -4,15 +4,34 @@ import statistic_util
 
 class TestCountSentences(unittest.TestCase):
     def test_empty_text(self):
-        self.assertEqual(statistic_util.count_sentences(''), 0, 'Empty text test: result isn\'t zero')
+        self.assertEqual(statistic_util.count_sentences(''), 0,
+                         'Empty text test: result isn\'t zero')
 
     def test_abbreviations(self):
         text = 'Hello, Mr. Derek. Sample text i.e. and more Ph.D. - it\'s greate text!!'
-        self.assertEqual(statistic_util.count_sentences(text), 2, 'Abbreviations test: number of sentences not 2')
+        self.assertEqual(statistic_util.count_sentences(text), 2,
+                         'Abbreviations test: number of sentences not 2')
 
     def test_sample_text(self):
         text = 'Oh!!! Thats good idea; Oh no... I forgot my keys in the car. '
-        self.assertEqual(statistic_util.count_sentences(text), 3, 'Sample text test: number of sentences not 3')
+        self.assertEqual(statistic_util.count_sentences(text), 3,
+                         'Sample text test: number of sentences not 3')
+
+
+class TestCountNonDeclarativeSentences(unittest.TestCase):
+    def test_empty_text(self):
+        self.assertEqual(statistic_util.count_non_declarative_sentences(''), 0,
+                         'Empty text test: result isn\'t zero')
+
+    def test_abbreviations(self):
+        text = 'Hello, Mr. Derek. Sample text i.e. and more Ph.D. - it\'s greate text!!'
+        self.assertEqual(statistic_util.count_non_declarative_sentences(text), 1,
+                         'Abbreviations test: number of non decl. sentences not 1')
+
+    def test_sample_text(self):
+        text = 'Oh!!! Thats good idea; Oh no... I forgot my keys in the car. '
+        self.assertEqual(statistic_util.count_non_declarative_sentences(text), 1,
+                         'Sample text test: number of non decl. sentences not 1')
 
 
 if __name__ == '__main__':
