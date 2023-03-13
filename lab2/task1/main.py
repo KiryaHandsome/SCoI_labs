cb484938b0820fa86c1c2756c04e678d51b92c62
kirya handsome <kirill19962003@gmail.com>
2023-03-13 17:22:41 +0300

    feat: add interact to task1/main

--- lab2/task1/main.py
+++ lab2/task1/main.py
@@ -1,11 +1,13 @@
-TEXT = '''
-Hello. It's test text for the second lab work!
-I hope my passing won't be too hard...
-'''
+import statistic_util
 
 
 def main():
-    print("Hello World!")
+    text = input('Enter text: ')
+    print('Amount of sentences: ' + str(statistic_util.count_sentences(text)))
+    print('Amount of non-declarative sentences: ' + str(statistic_util.count_non_declarative_sentences(text)))
+    print('Average word length: ' + str(statistic_util.get_avg_word_len(text)))
+    print('Average sentence length: ' + str(statistic_util.get_avg_sentence_len(text)))
+    print('Top k repeated anagrams: ' + str(statistic_util.get_top_k_repeated_n_grams(text)))
 
 
 if __name__ == "__main__":
